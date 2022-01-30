4adab4c62571bc70b030505a4576e0c720949864
siddh <siddh@Siddheswar>
2022-01-30 18:09:34 +0530

    for sai

---
+++ src/test/java/crmProject/Basic.java
@@ -0,0 +1,14 @@
+package crmProject;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+
+public class Basic {
+ @Test
+ public void basic() {
+	 WebDriver driver=new ChromeDriver();
+	 driver.get("https://www.flipkart.com/");
+	 
+ }
+}

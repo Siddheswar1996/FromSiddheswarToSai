b7f18b9cde262ae1dcaa9ec2e047b26dffbdeb14
siddh <siddh@Siddheswar>
2022-01-30 18:14:00 +0530

    for sai

--- src/test/java/crmProject/Basic.java
+++ src/test/java/crmProject/Basic.java
@@ -1,5 +1,6 @@
 package crmProject;
 
+import org.apache.poi.ss.usermodel.Workbook;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.annotations.Test;
@@ -9,6 +10,7 @@ public class Basic {
  public void basic() {
 	 WebDriver driver=new ChromeDriver();
 	 driver.get("https://www.flipkart.com/");
+	 Workbook wb=WorkbookFactory.
 	 
  }
 }
